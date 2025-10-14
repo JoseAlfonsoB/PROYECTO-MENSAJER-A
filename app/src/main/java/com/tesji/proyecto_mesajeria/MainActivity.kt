2e712c2199f02381fa1446248bcbf31cb771e0f0
TerrazasJr316 <terrazasjosue0@gmail.com>
2025-10-14 12:47:06 -0600

    cración del componente binding y evento buttonNV

--- app/src/main/java/com/tesji/proyecto_mesajeria/MainActivity.kt
+++ app/src/main/java/com/tesji/proyecto_mesajeria/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.tesji.proyecto_mesajeria
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tesji.proyecto_mesajeria.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNV.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.item_perfil->{
+                    true
+                }
+                R.id.item_usuarios->{
+                    true
+                }
+                R.id.item_chats->{
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
 
             // MINUTO DEL VÍDEO 9:00
-        }
     }
 }
