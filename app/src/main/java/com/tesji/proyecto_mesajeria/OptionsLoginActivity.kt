5409586932d9d479ec76f26bba8aea314d44951e
TerrazasJr316 <terrazasjosue0@gmail.com>
2025-10-15 21:26:24 -0600

    actualización logica con ActivityOptionsLoginBinding

--- app/src/main/java/com/tesji/proyecto_mesajeria/OptionsLoginActivity.kt
+++ app/src/main/java/com/tesji/proyecto_mesajeria/OptionsLoginActivity.kt
@@ -1,20 +1,25 @@
 package com.tesji.proyecto_mesajeria
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tesji.proyecto_mesajeria.databinding.ActivityOptionsLoginBinding
 
 class OptionsLoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOptionsLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_options_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityOptionsLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.optionEmail.setOnClickListener {
+            startActivity(Intent(applicationContext, LoginEmailActivity::class.java))
         }
+
     }
 }
