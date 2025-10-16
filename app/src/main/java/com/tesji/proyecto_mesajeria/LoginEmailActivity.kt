20782254ca75645e632e4ee04e787c8dfac48df1
TerrazasJr316 <terrazasjosue0@gmail.com>
2025-10-15 21:24:45 -0600

    creación del Empty Views Activity - Log in (kt & xml)

--- app/src/main/java/com/tesji/proyecto_mesajeria/LoginEmailActivity.kt
+++ app/src/main/java/com/tesji/proyecto_mesajeria/LoginEmailActivity.kt
@@ -1,20 +1,23 @@
 package com.tesji.proyecto_mesajeria
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tesji.proyecto_mesajeria.databinding.ActivityLoginEmailBinding
 
 class LoginEmailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginEmailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvRegister.setOnClickListener {
+            startActivity(Intent(applicationContext, RegistroEmailActivity::class.java))
         }
     }
 }
