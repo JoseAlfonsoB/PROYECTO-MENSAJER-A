7f5187109f44fea5d382d23c6fbf5c420f7fa081
TerrazasJr316 <terrazasjosue0@gmail.com>
2025-10-17 14:14:11 -0600

    dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
- plugins {
+plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
@@ -47,7 +47,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.constraintlayout)  
     // implementation(libs.androidx.navigation.fragment.ktx)
     // implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
